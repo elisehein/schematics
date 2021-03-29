c78b2bd9d141801d8e8bf7b6cc950012902889cd
Elise Hein <elisehein@gmail.com>
2021-03-29 19:01:12 +0300

    Add fig5 axis labels and play with spiral animation.

--- figures/Figure5.js
+++ figures/Figure5.js
@@ -1,5 +1,5 @@
 import Figure from "./Figure.js";
-import { Line, Path } from "./SVGElement.js";
+import { Line, Path, Text } from "./SVGElement.js";
 
 export default class Figure5 extends Figure {
   draw() {
@@ -9,14 +9,20 @@ export default class Figure5 extends Figure {
   }
 
   drawAxes() {
-    const yAxis = new Line({x: 149, y: 220}, {x: 149, y: 30});
-    this._canvas.appendChild(yAxis.node);
+    const timeAxis = new Line({x: 149, y: 220}, {x: 149, y: 30}, "arrowhead");
+    const timeLabel = new Text("Time", {x: 175, y: 35});
+    this._canvas.appendChild(timeAxis.node);
+    this._canvas.appendChild(timeLabel.node);
 
-    const xAxis = new Line({x: 149, y: 220}, {x: 250, y: 205});
+    const xAxis = new Line({x: 149, y: 220}, {x: 250, y: 205}, "arrowhead");
+    const xLabel = new Text("X", {x: 262, y: 205});
     this._canvas.appendChild(xAxis.node);
+    this._canvas.appendChild(xLabel.node);
 
-    const zAxis = new Line({x: 149, y: 220}, {x: 200, y: 260});
-    this._canvas.appendChild(zAxis.node);
+    const yAxis = new Line({x: 149, y: 220}, {x: 200, y: 260}, "arrowhead");
+    const yLabel = new Text("Y", {x: 214, y: 264});
+    this._canvas.appendChild(yAxis.node);
+    this._canvas.appendChild(yLabel.node);
   }
 
   drawSpiral() {
@@ -27,8 +33,8 @@ export default class Figure5 extends Figure {
                              C 90,175, 210,175, 210,135 \
                              C 210,95, 90,95, 90,115 \
                              C 90,135, 210,135, 210,95");
-    const spiralLength = 830;
-    document.documentElement.style.setProperty(`--${this._figureClass}-spiral-length`, spiralLength);
+
+    document.documentElement.style.setProperty(`--${this._figureClass}-spiral-length`, spiral.length);
 
     const node = spiral.node;
     node.classList.add(`${this._figureClass}__spiral`);
--- figures/SVGElement.js
+++ figures/SVGElement.js
@@ -1,28 +1,54 @@
 class SVGElement {
+  constructor(node) {
+    this._node = node;
+  }
+
   get node() {
     return this._node;
   }
 }
 
 export class Line extends SVGElement {
-  constructor(startPoint, endPoint) {
-    super();
+  constructor({ x: startX, y: startY }, { x: endX, y: endY }, markerDef = "") {
     const node = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    node.setAttribute("x1", startPoint.x);
-    node.setAttribute("y1", startPoint.y);
-    node.setAttribute("x2", endPoint.x);
-    node.setAttribute("y2", endPoint.y);
-    node.setAttribute("class", "line");
-    this._node = node;
+    node.setAttribute("x1", startX);
+    node.setAttribute("y1", startY);
+    node.setAttribute("x2", endX);
+    node.setAttribute("y2", endY);
+    node.classList.add("line");
+
+    if (markerDef) {
+      node.setAttribute("marker-end", `url(#${markerDef})`);
+    }
+
+    super(node);
   }
 }
 
 export class Path extends SVGElement {
   constructor(d) {
-    super();
     const node = document.createElementNS("http://www.w3.org/2000/svg", "path");
     node.setAttribute("d", d);
-    node.setAttribute("class", "line");
-    this._node = node;
+    node.classList.add("line");
+    super(node);
+  }
+
+  get length() {
+    return this._node.getTotalLength();
+  }
+}
+
+export class Text extends SVGElement {
+  constructor(text, { x, y }) {
+    const node = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    node.setAttribute("x", x);
+    node.setAttribute("y", y);
+
+    // Setting the font size via an attribute, we get the benefit of scaling
+    // according to the coordinate system specified in viewBox.
+    node.setAttribute("font-size", "10");
+    node.classList.add("text");
+    node.innerHTML = text;
+    super(node);
   }
 }
