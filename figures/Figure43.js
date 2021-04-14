0c01f861cf0d7d73b4693a6c93ed31ac46f5fcc9
Elise Hein <elisehein@gmail.com>
2021-04-14 15:39:07 +0300

    Remove previous approaches for fig. 43 animation and add easing.

--- figures/Figure43.js
+++ figures/Figure43.js
@@ -28,11 +28,10 @@ export default class Figure43 extends Figure {
 
   draw() {
     super.draw();
-    // this.drawCube();
-    this.drawSinglePathCube();
+    this.drawSinglePathCubeWithAnimation();
   }
 
-  drawSinglePathCube() {
+  drawSinglePathCubeWithAnimation() {
     const { A, B, C, D, E, F, G, H } = this._baseCoords;
 
     const pathDef = `
@@ -77,90 +76,14 @@ export default class Figure43 extends Figure {
     `;
 
     const rhombus = new Path(pathDef);
-    // const id = `rhombus-${rhombusDef}`;
-    // rhombus.node.setAttribute("id", id);
 
     rhombus.node.innerHTML = `
     <animate
       attributeName="d"
       values="${pathDef};${skewedPathDef};${pathDef}"
-      dur="10s"
-      begin="0s"
-      repeatCount="indefinite"
-      fill="freeze" />
-    `;
-
-    this.addSVGChildElement(rhombus.node);
-  }
-
-  drawCube() {
-    this.drawLines([
-      { from: "A", to: "E", className: "line--moving-left" },
-      { from: "B", to: "F", className: "line--moving-left" },
-      { from: "C", to: "G", className: "line--moving-right" },
-      { from: "D", to: "H", className: "line--moving-right" }
-    ]);
-
-    this.drawSkewingRhombi([
-      "ABDC",
-      "EFHG"
-    ]);
-  }
-
-  drawLines(lineDefs) {
-    lineDefs.forEach(this.drawLine.bind(this));
-  }
-
-  drawLine(lineDef) {
-    // const line = new Line(this._baseCoords[lineDef.from], this._baseCoords[lineDef.to]);
-    const id = `line-${lineDef.from}${lineDef.to}`;
-    const from = this._baseCoords[lineDef.from];
-    const to = this._baseCoords[lineDef.to];
-
-    const line = new Path(`M${from.x},${from.y}L${to.x},${to.y}`);
-    line.node.classList.add(lineDef.className);
-    line.node.setAttribute("id", id);
-
-    // const animate = createSVGElement("animate");
-    // line.node.innerHTML = `
-    // <animate attributeName="d" from="M50,100L100,100L150,100" to="M50,100L100,150L150,100" dur="2s" begin="mycross.click" fill="freeze" />
-    // `;
-
-    this.addSVGChildElement(line.node);
-  }
-
-  drawSkewingRhombi(rhombusDefs) {
-    rhombusDefs.forEach(this.drawRhombus.bind(this));
-  }
-
-  drawRhombus(rhombusDef) {
-    const pointsInOrder = rhombusDef.split("").map(point => this._baseCoords[point]);
-
-    const pathDef = `
-    M${pointsInOrder[0].x},${pointsInOrder[0].y}
-    L${pointsInOrder[1].x},${pointsInOrder[1].y}
-    M${pointsInOrder[2].x},${pointsInOrder[2].y}
-    L${pointsInOrder[3].x},${pointsInOrder[3].y}
-    Z
-    `;
-
-    const skewAmount = this._cubeFaceSize / 2;
-    const skewedPathDef = `
-    M${pointsInOrder[0].x - skewAmount},${pointsInOrder[0].y}
-    L${pointsInOrder[1].x - skewAmount},${pointsInOrder[1].y}
-    M${pointsInOrder[2].x + skewAmount},${pointsInOrder[2].y}
-    L${pointsInOrder[3].x + skewAmount},${pointsInOrder[3].y}
-    Z
-    `;
-
-    const rhombus = new Path(pathDef);
-    const id = `rhombus-${rhombusDef}`;
-    rhombus.node.setAttribute("id", id);
-
-    rhombus.node.innerHTML = `
-    <animate
-      attributeName="d"
-      values="${pathDef};${skewedPathDef};${pathDef}"
+      keyTimes="0;0.5;1"
+      calcMode="spline"
+      keySplines="0.42 0 0.58 1; 0.42 0 0.58 1"
       dur="10s"
       begin="0s"
       repeatCount="indefinite"
