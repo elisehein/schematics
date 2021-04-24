a1a783a35c606d1ea461798eb29f5843f2cda7af
Elise Hein <elisehein@gmail.com>
2021-04-24 11:22:21 +0300

    Begin fleshing out fig. 36

--- figures/Figure36.js
+++ figures/Figure36.js
@@ -1,8 +1,24 @@
 import Figure from "./Figure.js";
+import { Line } from "./SVGShape.js";
 
 export default class Figure36 extends Figure {
   constructor() {
     super(36);
+  }
+
+  draw() {
+    super.draw();
+    this.drawPendulum();
+  }
+
+  drawPendulum() {
+    const pendulum = new Line(
+      { x: 30, y: 240 },
+      { x: 150, y: 60 },
+      { x: 270, y: 240 }
+    );
+
+    this.addSVGChildElement(pendulum.node);
   }
 }
 
