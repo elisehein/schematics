1418bebafa5d2034e4dd6f86d2c5132033fcb771
Elise Hein <elisehein@gmail.com>
2021-05-02 21:28:28 +0300

    Tidy and simplify figure navigation.

--- main.js
+++ main.js
@@ -54,6 +54,25 @@ function updateNavigation(newFigureNum) {
     figureLink.classList.remove("active");
   });
   document.querySelector(`[data-figure-link="${newFigureNum}"]`).classList.add("active");
+
+  const prevLink = document.getElementById("previous-figure-link");
+  const nextLink = document.getElementById("next-figure-link");
+
+  const newFigureNumIndex = orderedFigures.indexOf(newFigureNum);
+  const prevFigureNum = orderedFigures[newFigureNumIndex - 1];
+  const nextFigurNum = orderedFigures[newFigureNumIndex + 1];
+
+  configureDirectionalFigureLink(prevLink, prevFigureNum);
+  configureDirectionalFigureLink(nextLink, nextFigurNum);
+}
+
+function configureDirectionalFigureLink(node, num) {
+  if (num) {
+    node.setAttribute("href", `#fig${num}`);
+  } else {
+    node.setAttribute("aria-hidden", true);
+    node.removeAttribute("href");
+  }
 }
 
 nav.init({ defaultFigureNum });
