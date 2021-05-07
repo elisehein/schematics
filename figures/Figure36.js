4ee84ce31daa6abce33c3aea23d95c9ac5cdb3be
Elise Hein <elisehein@gmail.com>
2021-05-07 09:21:54 +0300

    Add user-initiated swing to Figure 36.

--- figures/Figure36.js
+++ figures/Figure36.js
@@ -11,7 +11,12 @@ export default class Figure36 extends Figure {
     this._pendulumLength = 200;
     this._markerRadius = 20;
     this._initialAngle = 30;
-    this._anchorPoint = { x: 150, y: (300 - this._pendulumLength) / 2 };
+
+    // -10 in the end is to nudge it slightly towards the top to make room for
+    // the dashed arrow
+    const offset = 10;
+    this._anchorPoint = { x: 150, y: (300 - this._pendulumLength) / 2 - offset };
+
     this._swingDuration = 2;
     this._totalSwings = 30;
     this._angleChangeStep = this._initialAngle / (this._totalSwings - 1);
@@ -21,20 +26,28 @@ export default class Figure36 extends Figure {
     super.draw();
     this.defineCircleMarker({ anchor: false });
     this.defineCircleMarker({ anchor: true });
-    this.drawSwingingArm();
+    this.drawDashedArrow();
 
-    const arcRadius = this._pendulumLength + (this._markerRadius * 2);
-    const arc = new Arc(this._anchorPoint, arcRadius, 150, 210);
-    arc.stroke();
-    this.addSVGChildElement(arc.node);
+    const swingingArm = this.drawSwingingArm();
 
-    setTimeout(() => {
-      this.drawStaticArm();
-    }, this._swingDuration * 1000);
+    const pointAtMarker = swingingArm.node.getPointAtLength(this._pendulumLength);
+    pointAtMarker.y += this._markerRadius;
+
+    const markerClickOverlay = this.drawMarkerClickOverlay(pointAtMarker);
+    markerClickOverlay.node.addEventListener("click", () => this.swing(swingingArm, markerClickOverlay));
   }
 
-  rotateArm(armNode, angle) {
-    armNode.setAttribute(
+  drawDashedArrow() {
+    const arcRadius = this._pendulumLength + (this._markerRadius * 2) + 10;
+    const arc = new Arc(this._anchorPoint, arcRadius, 160, 200);
+    arc.stroke();
+    arc.addArrowHead(this.registerMarker.bind(this));
+    arc.node.classList.add("arrow");
+    this.addSVGChildElement(arc.node);
+  }
+
+  addRotation(node, angle) {
+    node.setAttribute(
       "transform",
       `rotate(${angle} ${this._anchorPoint.x} ${this._anchorPoint.y})`
     );
@@ -49,21 +62,40 @@ export default class Figure36 extends Figure {
     }
   }
 
-  drawStaticArm() {
-    const staticArm = this.getDownwardArm();
-    this.rotateArm(staticArm.node, (this._initialAngle - this._angleChangeStep) * -1);
-    this.setMarkers(staticArm.node);
-    this.addSVGChildElement(staticArm.node);
-  }
-
   drawSwingingArm() {
     const swingingArm = this.getDownwardArm();
     this.setMarkers(swingingArm.node, true);
-    this.rotateArm(swingingArm.node, this._initialAngle);
-
-    swingingArm.node.innerHTML = this.getAnimationNodeString();
-
+    this.addRotation(swingingArm.node, this._initialAngle);
+    swingingArm.node.innerHTML = this.getSwingAnimationNodeString();
     this.addSVGChildElement(swingingArm.node);
+    return swingingArm;
+  }
+
+  drawMarkerClickOverlay({ x, y }) {
+    const markerClickOverlay = this.getMarkerCircle(x, y);
+    markerClickOverlay.node.classList.add("marker-click-overlay");
+    this.addRotation(markerClickOverlay.node, this._initialAngle);
+    markerClickOverlay.node.innerHTML = this.getPulseAnimationNodeString();
+
+    this.addSVGChildElement(markerClickOverlay.node);
+
+    return markerClickOverlay;
+  }
+
+  swing(swingingArm, markerClickOverlay) {
+    swingingArm.node.querySelector("animateTransform").beginElement();
+    markerClickOverlay.node.remove();
+
+    setTimeout(() => {
+      this.drawStaticArm();
+    }, this._swingDuration * 1000);
+  }
+
+  drawStaticArm() {
+    const staticArm = this.getDownwardArm();
+    this.addRotation(staticArm.node, (this._initialAngle - this._angleChangeStep) * -1);
+    this.setMarkers(staticArm.node);
+    this.addSVGChildElement(staticArm.node);
   }
 
   getDownwardArm() {
@@ -88,14 +120,19 @@ export default class Figure36 extends Figure {
       x: markerX,
       y: markerY
     });
-    const circle = new Circle(this._markerRadius + safeArea, this._markerRadius + safeArea, this._markerRadius);
-    circle.stroke();
+    const circle = this.getMarkerCircle(this._markerRadius + safeArea, this._markerRadius + safeArea);
 
     marker.addShape(circle.node);
     this.registerMarker(marker.node);
   }
 
-  getAnimationNodeString() {
+  getMarkerCircle(cx, cy) {
+    const circle = new Circle(cx, cy, this._markerRadius);
+    circle.stroke();
+    return circle;
+  }
+
+  getSwingAnimationNodeString() {
     const easeInOut = ".4 0 .6 1"
 
     const rotationValue = deg => `${deg} ${this._anchorPoint.x} ${this._anchorPoint.y}`;
@@ -120,9 +157,33 @@ export default class Figure36 extends Figure {
       calcMode="spline"
       keySplines="${Array(this._totalSwings - 1).fill(easeInOut).join("; ")}"
       dur="${this._totalSwings * this._swingDuration}s"
-      begin="0s"
+      begin="indefinite"
       repeatCount="1"
       fill="freeze" />
+    `;
+  }
+
+  getPulseAnimationNodeString() {
+    const keyTimes = "0; 0.3; 1";
+    const duration = "2s";
+    const begin = "2s;"
+
+    return `
+    <animate
+      attributeName="r"
+      begin="${begin}"
+      dur="${duration}"
+      repeatCount="indefinite"
+      values="${this._markerRadius}; ${this._markerRadius * 1.6}; ${this._markerRadius}"
+      keyTimes="${keyTimes}" />
+    <animate
+      attributeType="xml"
+      attributeName="stroke-opacity"
+      begin="${begin}"
+      dur="${duration}"
+      values="1; 0; 0"
+      keyTimes="${keyTimes}"
+      repeatCount="indefinite" />
     `;
   }
 }
