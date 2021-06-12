59188692ad416b0121788b94057cc55fffe17f1f
Elise Hein <elisehein@gmail.com>
2021-06-12 14:35:49 +0300

    GH Actions workflow for linting and testing (#7)
    * Add lint and test GH workflow.
    
    * Add .eslintignore to ignore test files.
    
    * Fix some eslint errors.
    
    * Fix some stylelint issues.

--- src/helpers/arcCalculations.js
+++ src/helpers/arcCalculations.js
@@ -5,12 +5,12 @@ export const polarToCartesian = (x, y, radius, angleInDegrees) => {
     x: x + (radius * Math.cos(angleInRadians)),
     y: y + (radius * Math.sin(angleInRadians))
   };
-}
+};
 
-export const getArcPathD = ({ x, y, radius}, { startAngle, endAngle }) => {
+export const getArcPathD = ({ x, y, radius }, { startAngle, endAngle }) => {
   const start = polarToCartesian(x, y, radius, endAngle);
   const end = polarToCartesian(x, y, radius, startAngle);
   const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
 
   return `M ${start.x} ${start.y} A ${radius} ${radius} 0 ${largeArcFlag} 0 ${end.x} ${end.y}`;
-}
+};
