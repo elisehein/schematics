0a4a1da1c203d15fe7c3224edf5f0ba7264f0d97
Elise Hein <elisehein@gmail.com>
2021-06-25 10:46:42 +0300

    Remove unused logic and transfer pointer state management to PointerEvents.

--- src/components/Diagram/Figure20Diagram/Figure20PointerEvents.js
+++ src/components/Diagram/Figure20Diagram/Figure20PointerEvents.js
@@ -1,11 +1,17 @@
+const pointerStates = {
+  ENTERED_BUT_NOT_MOVED: "ENTERED_BUT_NOT_MOVED",
+  ENTERED_AND_MOVED: "ENTERED_AND_MOVED",
+  NOT_ENTERED: "NOT_ENTERED"
+};
+
 export default class Figure20PointerEvents {
   constructor(svgNode) {
     this._svgNode = svgNode;
     this._svgReferencePoint = svgNode.createSVGPoint();
+    this._state = pointerStates.NOT_ENTERED;
   }
 
   respondToPointer({ positionRespondsToMovement, onEnter, onMove, onLeave }) {
-    this._respondingToMovement = false;
     this.respondToMouseLeave(onLeave);
     this.respondToMouseMove({ positionRespondsToMovement, onEnter, onMove, onLeave });
   }
@@ -15,25 +21,25 @@ export default class Figure20PointerEvents {
       const pointerPosition = this.getPointerPositionInSVG(event);
 
       if (!positionRespondsToMovement(pointerPosition)) {
-        this._respondingToMovement = false;
+        this._state = pointerStates.NOT_ENTERED;
         onLeave(pointerPosition);
         return;
       }
 
-      if (this._respondingToMovement) {
+      if (this.respondingToMovement) {
         onMove(pointerPosition);
+        this._state = pointerStates.ENTERED_AND_MOVED;
       } else {
+        this._state = pointerStates.ENTERED_BUT_NOT_MOVED;
         onEnter(pointerPosition);
       }
-
-      this._respondingToMovement = true;
     });
   }
 
   respondToMouseLeave(onLeave) {
     this._svgNode.addEventListener("mouseleave", event => {
-      if (this._respondingToMovement) {
-        this._respondingToMovement = false;
+      if (this.respondintToMovement) {
+        this._state = pointerStates.NOT_ENTERED;
         onLeave(this.getPointerPositionInSVG(event));
       }
     });
@@ -44,4 +50,13 @@ export default class Figure20PointerEvents {
     this._svgReferencePoint.y = event.clientY;
     return this._svgReferencePoint.matrixTransform(this._svgNode.getScreenCTM().inverse());
   }
+
+  get pointerJustEntered() {
+    return this._state == pointerStates.ENTERED_BUT_NOT_MOVED;
+  }
+
+  get respondingToMovement() {
+    return this._state !== pointerStates.NOT_ENTERED;
+  }
 }
+
